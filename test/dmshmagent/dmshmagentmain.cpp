63385d57011700569a4d27d7576f487777f22708
brinkqiang <brink.qiang@gmail.com>
2021-05-12 23:58:07 +0800

    add

--- test/dmshmagent/dmshmagentmain.cpp
+++ test/dmshmagent/dmshmagentmain.cpp
@@ -8,13 +8,17 @@ int main( int argc, char* argv[] )
 {
     DMSHM_AGENT_INIT();
     fmt::print("{:%Y-%m-%d %H:%M:%S}\n", fmt::localtime(time(NULL)));
-    for (int i=0; i < 100; ++i)
+    for (int i=0; i < 10; ++i)
     {
         SHM_WRITE(fmt::format("{}", i), fmt::format("this is a test {}", i));
     }
+    for (int i = 0; i < 10; ++i)
+    {
+        std::string msg;
+        SHM_READ(std::to_string(i), msg);
+        fmt::print("{} = {}\n", i, msg);
+    }
 
-    std::string msg;
-    SHM_READ("7", msg);
     fmt::print("{:%Y-%m-%d %H:%M:%S}\n", fmt::localtime(time(NULL)));
     return 0;
 }
